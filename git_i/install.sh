61c7c1d1dafdb623c7ff8509ef05acaf7a270374
Jack DuPlessis <jduplessis294@gmail.com>
2017-06-12 23:25:33 -0400

    Add rnbw fix

--- git_i/install.sh
+++ git_i/install.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 command -v git >/dev/null 2>&1 || { echo >&2 "I require git but its not installed. Installing."; sudo apt-get install git;}
-ln -s $DOT_FILE_DIRECTORY/git/.gitconfig ~/.gitconfig
+ln -s $INSTALL_DATA_DIR/git/.gitconfig ~/.gitconfig
