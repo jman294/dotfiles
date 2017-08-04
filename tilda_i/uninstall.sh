f2ec31ced2c553c6f9026ecd8837a2d2a9a873be
Jack DuPlessis <jduplessis294@gmail.com>
2017-08-04 16:45:52 -0400

    Fix tilda uninstaller

--- tilda_i/uninstall.sh
+++ tilda_i/uninstall.sh
@@ -1,5 +1,2 @@
 #!/bin/bash
-rm ~/.fonts/PowerlineSymbols.*
-rm ~/.config/fontconfig/conf.d/10-powerline-symbols.*
-[ -s ~/.vimrc ] && rm ~/.vimrc
-[ -d ~/.vim/bundle ] && rm -rf ~/.vim/bundle/
+sudo apt-get purge tilda
