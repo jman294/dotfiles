6dede54aacd8dce5bed49b7e0e685112966c3b32
Jack DuPlessis <jduplessis294@gmail.com>
2017-09-23 14:12:26 -0400

    Fix tilda install

--- tilda_i/install.sh
+++ tilda_i/install.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
-if [ -e ~/.vimrc ]; then
-  echo "You already have a .vimrc in ~/, renaming it to .vimrc.before\n"
-  mv ~/.vimrc ~/.vimrc.before
-fi
-
-# Install vim if it is not installed
+# Install tilda if it is not installed
 command -v tilda >/dev/null 2>&1 || { echo >&2 "I require tilda but its not installed. Installing."; sudo apt-get install tilda;}
 
 mkdir -p ~/.config/tilda/
