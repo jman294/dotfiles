a1aa1d530488633e40c8ff834a416d318b8424ce
Jack DuPlessis <jduplessis294@gmail.com>
2017-09-30 14:08:55 -0400

    Add i3 config files

---
+++ i3blocks_i/install.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+# Link .Xmodmap file in this directory to the home directory
+ln -s $INSTALL_DATA_DIR/i3blocks/config ~/.config/i3blocks/config
---
+++ i3config_i/install.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+# Link .Xmodmap file in this directory to the home directory
+ln -s $INSTALL_DATA_DIR/i3/config ~/.config/i3/config
