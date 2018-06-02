1ecb635987ecc94908c0e0b36bb7150284955df0
Jack DuPlessis <jduplessis294@gmail.com>
2018-06-01 21:26:11 -0400

    Add global gitignore

--- git_i/install.sh
+++ git_i/install.sh
@@ -2,3 +2,4 @@
 
 command -v git >/dev/null 2>&1 || { echo >&2 "I require git but its not installed. Installing."; sudo apt-get install git;}
 ln -s $INSTALL_DATA_DIR/git/.gitconfig ~/.gitconfig
+ln -s $INSTALL_DATA_DIR/git/.gitignore_global ~/.gitignore_global
