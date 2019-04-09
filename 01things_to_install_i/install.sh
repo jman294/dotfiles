29b74b6092cc3168cd1f765c4d91bd3388b72b7f
Jack DuPlessis <jduplessis294@gmail.com>
2019-04-08 22:34:41 -0400

    Update

--- 01things_to_install_i/install.sh
+++ 01things_to_install_i/install.sh
@@ -1,5 +1,6 @@
-sudo apt-get install cowsay git vim curl thunderbird fortune tilda jq fontconfig libfontconfig1 libfontconfig1-dev
-sudo add-apt-repository universe
-sudo add-apt-repository ppa:inkscape.dev/stable
-sudo apt-get update
-sudo apt install inkscape
+#sudo apt-get install cowsay git vim curl thunderbird fortune tilda jq fontconfig libfontconfig1 libfontconfig1-dev
+#sudo add-apt-repository universe
+#sudo add-apt-repository ppa:inkscape.dev/stable
+#sudo apt-get update
+#sudo apt install inkscape
+pacman -S jq cowsay fortune
